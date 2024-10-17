a3e49807b68602263a715b5cfad30cc5e3a93a0c
JiangweiYu221 <152498452+JiangweiYu221@users.noreply.github.com>
2024-10-17 10:48:18 +0800

    Add files via upload

---
+++ artifact/api.py
@@ -0,0 +1,13 @@
+from neuracle_lib.triggerBox import TriggerBox
+
+_trigger_box = None
+
+def mark(index:int):
+    # _trigger_box.output_event_data(index)
+    pass
+
+
+def start():
+    global _trigger_box
+    _trigger_box = TriggerBox("COM4")
+    mark(0)
